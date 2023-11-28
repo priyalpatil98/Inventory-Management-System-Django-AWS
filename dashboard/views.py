c6baed5c5d013cab200b7b2f348b23f4af1e2a40
priyal-cloud98 <x22209573@student.ncirl.ie>
2023-11-28 00:47:58 +0000

    AWS S3 Implementation for media files

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,9 +10,9 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     orders = Order.objects.all()
     products = Product.objects.all()
-    orders_count = orders.count()
-    products_count = products.count()
-    workers_count = User.objects.all().count()
+    orders_count = orders.count() # To get Orders count to display on Admin Dashboard
+    products_count = products.count() # To get  products count to display on Admin Dashboard
+    workers_count = User.objects.all().count() # To get Staff's count to display on Admin Dashboard
     
     if request.method == 'POST':
         form = OrderForm(request.POST)
@@ -37,9 +37,9 @@ def index(request):
 @login_required(login_url='user-login')
 def staff(request):
     workers = User.objects.all()
-    workers_count = workers.count()
-    orders_count = Order.objects.all().count()
-    products_count = Product.objects.all().count()
+    workers_count = workers.count() # To get Staff's count to display on Admin Dashboard
+    orders_count = Order.objects.all().count() # To get Orders count to display on Admin Dashboard
+    products_count = Product.objects.all().count() # To get  products count to display on Admin Dashboard
     context = {
         'workers' : workers,
         'workers_count' : workers_count,
@@ -52,9 +52,9 @@ def staff(request):
 @login_required(login_url='user-login')
 def product(request):
     items = Product.objects.all() #ORM 'SELECT * FROM dashboard_product'
-    products_count = items.count()
-    workers_count = User.objects.all().count()
-    orders_count = Order.objects.all().count()
+    products_count = items.count() # To get  products count to display on Admin Dashboard
+    workers_count = User.objects.all().count() # To get Staff's count to display on Admin Dashboard
+    orders_count = Order.objects.all().count() # To get Orders count to display on Admin Dashboard
 
     if request.method == 'POST':
         form = ProductForm(request.POST)
@@ -106,9 +106,9 @@ def product_update(request, pk):
 @login_required(login_url='user-login')
 def order(request):
     orders = Order.objects.all()
-    orders_count = Order.objects.count()
-    workers_count = User.objects.all().count()
-    products_count = Product.objects.all().count()
+    orders_count = Order.objects.count() # To get Orders count to display on Admin Dashboard
+    workers_count = User.objects.all().count() # To get Staff's count to display on Admin Dashboard
+    products_count = Product.objects.all().count() # To get  products count to display on Admin Dashboard
     context = {
         'orders' : orders,
         'workers_count' : workers_count,
@@ -121,8 +121,16 @@ def order(request):
 @login_required(login_url='user-login')
 def staff_detail(request, pk):
     worker = User.objects.get(id=pk)
+    workers = User.objects.all()
+    workers_count = workers.count() # To get Staff's count to display on Admin Dashboard
+    orders_count = Order.objects.all().count() # To get Orders count to display on Admin Dashboard
+    products_count = Product.objects.all().count() # To get  products count to display on Admin Dashboard
     context = {
-        'worker' : worker
+        'worker' : worker,
+        'workers' : workers,
+        'workers_count' : workers_count,
+        'orders_count' : orders_count,
+        'products_count' : products_count,
     }
     return render(request, 'dashboard/staff_detail.html', context)
 
